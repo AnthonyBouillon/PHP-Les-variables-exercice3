b6e3e0bfcce8686e5ff0caf1848f2fb03acef33b
Anthony <anthony_bouillon@outlook.fr>
2017-12-12 16:30:45 +0100

    Les variables

--- index.php
+++ index.php
@@ -1,8 +1,23 @@
-<?php
-$km = 1;
-echo $km. ' ';
-$km = 3;
-echo $km. ' ';
-$km = 125;
-echo $km;
- ?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Exercice 3</title>
+  </head>
+  <body>
+    <?php
+      $km = 1;
+      echo $km;
+      ?>
+      <br/>
+      <?php
+      $km = 3;
+      echo $km;
+      ?>
+      <br/>
+      <?php
+      $km = 125;
+      echo $km;
+    ?>
+  </body>
+</html>
